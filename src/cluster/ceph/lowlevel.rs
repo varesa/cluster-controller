b83cb41224f54bd2406c18287168280328dc1bfa
Esa Varemo <esa@kuivanto.fi>
2021-01-11 02:24:39 +0200

    Experiment: List RBD images

--- src/cluster/ceph/lowlevel.rs
+++ src/cluster/ceph/lowlevel.rs
@@ -1,15 +1,35 @@
-use librados_sys::{rados_create, rados_t, rados_conf_set, rados_conf_read_file, rados_connect, rados_pool_list};
-use std::ptr::null;
+use librados_sys::{
+    rados_t,
+    rados_ioctx_t,
+    rados_create,
+    rados_conf_set,
+    rados_conf_read_file,
+    rados_connect,
+    rados_ioctx_create,
+    rados_pool_list
+};
+
+use librbd_sys::{
+    rbd_list,
+};
 
 use crate::errors::{RadosError, Error};
 use std::ffi::CString;
 
+macro_rules! call {
+    ($operation:literal, $rados_call:expr) => {
+        let code = $rados_call;
+        if code < 0 {
+            return Err(RadosError { operation: String::from($operation), code}.into());
+        }
+    }
+}
 
 pub fn connect() -> Result<rados_t, Error>{
     unsafe {
         let mut cluster: rados_t = 0 as rados_t;
 
-        // Free at end
+        // Must be returned and freed at the end
         let user_c = CString::new("clusteradmin-dev")
             .expect("Failed to create CString").into_raw();
         let opt_keyring_c = CString::new("keyring")
@@ -19,25 +39,10 @@ pub fn connect() -> Result<rados_t, Error>{
         let conf_c = CString::new("ceph/ceph.conf")
             .expect("Failed to create CString").into_raw();
 
-        let code = rados_create(&mut cluster, user_c);
-        if code != 0 {
-            return Err(RadosError {operation: String::from("rados_create"), code}.into());
-        }
-
-        let code = rados_conf_read_file(cluster, conf_c);
-        if code != 0 {
-            return Err(RadosError {operation: String::from("rados_conf_read_file"), code}.into());
-        }
-
-        let code = rados_conf_set(cluster, opt_keyring_c, keyring_c);
-        if code != 0 {
-            return Err(RadosError {operation: String::from("rados_conf_set keyring"), code}.into());
-        }
-
-        let code = rados_connect(cluster);
-        if code != 0 {
-            return Err(RadosError {operation: String::from("rados_connect"), code}.into());
-        }
+        call!("rados_create", rados_create(&mut cluster, user_c));
+        call!("rados_conf_read_file", rados_conf_read_file(cluster, conf_c));
+        call!("rados_conf_set", rados_conf_set(cluster, opt_keyring_c, keyring_c));
+        call!("rados_conect", rados_connect(cluster));
 
         // Return control to rust for freeing the memory
         CString::from_raw(user_c);
@@ -52,14 +57,44 @@ pub fn list_pools(cluster: rados_t) -> Result<(), Error> {
     let mut buffer = vec![0 as u8; 1024];
     let buffer_len = buffer.len();
     unsafe {
-        //let buffer_c = CString::from_vec_unchecked(buffer).into_raw();
         let code = rados_pool_list(cluster, buffer.as_mut_ptr() as *mut i8, buffer_len);
-        //let buffer_s = CString::from_raw(buffer_c);
     }
     let pools = buffer.split(|c| { *c == 0 });
     for pool in pools {
         if pool.len() == 0 { break; }
         println!("{}", std::str::from_utf8(pool).expect("Failed to convert name to string"));
+    }
+    Ok(())
+}
+
+pub fn get_pool(cluster: rados_t, pool_name: String) -> Result<rados_ioctx_t, Error> {
+    let mut pool: rados_ioctx_t = 0 as rados_ioctx_t;
+
+    unsafe {
+        // Must be returned and freed at the end
+        let pool_name_c = CString::new(pool_name)
+            .expect("Failed to create CString").into_raw();
+
+        call!("rados_ioctx_create", rados_ioctx_create(cluster, pool_name_c, &mut pool));
+
+        // Take back control to free memory
+        CString::from_raw(pool_name_c);
+    }
+    Ok(pool)
+}
+
+pub fn list_images(pool: rados_ioctx_t) -> Result<(), Error> {
+    let mut buffer = vec![0 as u8; 1024];
+    let mut buffer_len: libc::size_t = buffer.len();
+
+    unsafe {
+        call!("rbd_list", rbd_list(pool, buffer.as_mut_ptr() as *mut i8, &mut buffer_len));
+    }
+
+    let images = buffer.split(|c| { *c == 0});
+    for image in images {
+        if image.len() == 0 { break; }
+        println!("{}", std::str::from_utf8(image).expect("Failed to conver image to string"));
     }
     Ok(())
 }
--- src/cluster/ceph/mod.rs
+++ src/cluster/ceph/mod.rs
@@ -5,5 +5,7 @@ use crate::errors::Error;
 pub async fn run() -> Result<(), Error> {
     let mut cluster = lowlevel::connect()?;
     lowlevel::list_pools(cluster)?;
+    let mut pool = lowlevel::get_pool(cluster, "volumes".into())?;
+    lowlevel::list_images(pool)?;
     Ok(())
 }
