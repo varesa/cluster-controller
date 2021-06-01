0a260a65e7ba96e7c1c76c226e84526b420b5d47
Esa Varemo <git@esav.fi>
2021-06-01 05:37:53 +0300

    Capture a single date for image

--- push.sh
+++ push.sh
@@ -4,14 +4,15 @@ set -euo pipefail
 
 TMP="tmp-cluster-controller-build"
 REMOTE="registry.acl.fi/public/virt-controller"
+DATE="$(date +%Y%m%d-%H%M%S)"
 
 podman build -t "$TMP" .
 version="$(podman run --rm tmp-cluster-controller-build cluster-controller --version)"
 
 podman tag "$TMP" "$REMOTE:$version"
-podman tag "$TMP" "$REMOTE:$(date +%Y%m%d-%H%M%S)"
+podman tag "$TMP" "$REMOTE:$DATE"
 podman tag "$TMP" "$REMOTE:latest"
 
 podman push "$REMOTE:$version"
-podman push "$REMOTE:$(date +%Y%m%d-%H%M%S)"
+podman push "$REMOTE:$DATE"
 podman push "$REMOTE:latest"
