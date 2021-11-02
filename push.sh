653330d94be1635c8a321fc01192e3e67cc8d29a
Esa Varemo <git@esav.fi>
2021-11-02 21:22:17 +0200

    Do not tag commit on unclean tree

--- push.sh
+++ push.sh
@@ -6,18 +6,30 @@ TMP="tmp-cluster-controller-build"
 REMOTE="registry.acl.fi/public/virt-controller"
 DATE="$(date +%Y%m%d-%H%M%S)"
 
+if [ "$(git status --porcelain)" == "" ]; then
+    CLEAN=true
+else
+    CLEAN=false
+fi
+
 podman build -t "$TMP" .
 version="$(podman run --rm tmp-cluster-controller-build cluster-controller --version)"
 
-podman tag "$TMP" "$REMOTE:$version"
+[ "$CLEAN" == "true" ] && podman tag "$TMP" "$REMOTE:$version"
 podman tag "$TMP" "$REMOTE:$DATE"
 podman tag "$TMP" "$REMOTE:latest"
 
-podman push "$REMOTE:$version"
+[ "$CLEAN" == "true" ] && podman push "$REMOTE:$version"
 podman push "$REMOTE:$DATE"
 podman push "$REMOTE:latest"
+
+if [ "$CLEAN" == "true" ]; then
+    IMG="$REMOTE:$version"
+else
+    IMG="$REMOTE:$DATE"
+fi
 
 kubectl set image \
     -n virt-controller \
     deployment/cluster-controller \
-    cluster-controller="$REMOTE:$version"
+    cluster-controller="$IMG"
