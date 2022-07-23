edda9f3561b0f96fd3cee38faec2be471d33937b
Esa Varemo <git@esav.fi>
2022-07-23 16:51:12 +0300

    rustfmt project

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
-use std::process::Command;
-use std::fs;
 use regex::Regex;
+use std::fs;
+use std::process::Command;
 
 fn main() {
-    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
+    let output = Command::new("git")
+        .args(&["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
     let git_hash = String::from_utf8(output.stdout).map_or("undetermined".into(), |o| o);
 
-    let output = Command::new("git").args(&["rev-list", "--count", "HEAD"]).output().unwrap();
+    let output = Command::new("git")
+        .args(&["rev-list", "--count", "HEAD"])
+        .output()
+        .unwrap();
     let git_count = String::from_utf8(output.stdout).map_or("undetermined".into(), |o| o);
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
@@ -16,8 +22,10 @@ fn main() {
     if let Ok(head) = fs::read_to_string(".git/HEAD") {
         let re = Regex::new(r"ref: (.*)").unwrap();
         if let Some(captures) = re.captures(&head) {
-            println!("cargo:rustc-rerun-if-changed=.git/{}",
-                     captures.get(1).map_or("", |m| m.as_str()));
+            println!(
+                "cargo:rustc-rerun-if-changed=.git/{}",
+                captures.get(1).map_or("", |m| m.as_str())
+            );
         }
     }
 }
