b9525559252252db7ffc9773cad6b02c4ca45535
Esa Varemo <esa@kuivanto.fi>
2022-07-28 03:01:13 +0300

    UUID is the second, not the first element of the array

--- src/cluster/ovn/logicalswitch.rs
+++ src/cluster/ovn/logicalswitch.rs
@@ -106,7 +106,7 @@ impl OvnCommon for LogicalSwitch {
             uuid: try_deserialize!(object
                 .get("_uuid")
                 .and_then(|a| a.as_array())
-                .and_then(|a| a.get(0))
+                .and_then(|a| a.get(1))
                 .and_then(|u| u.as_str()))
             .to_owned(),
             name: try_deserialize!(object.get("name").and_then(|u| u.as_str())).to_owned(),
