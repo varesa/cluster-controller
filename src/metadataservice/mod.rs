b3d48ca62d9e5aeb56d2c1a51ccc400fd74de0b9
Esa Varemo <esa@kuivanto.fi>
2022-07-29 18:43:09 +0300

    Get inner result from proxy thread

--- src/metadataservice/mod.rs
+++ src/metadataservice/mod.rs
@@ -29,7 +29,8 @@ pub async fn run(args: Vec<String>, client: Client) -> Result<(), Error> {
     let (ch_backend, ch_proxy) = bidirectional_channel::<ChannelProtocol>();
 
     let proxy_thread = std::thread::spawn(move || MetadataProxy::run(ch_proxy, &ns_name));
-    let proxy_task = tokio::task::spawn_blocking(|| proxy_thread.join());
+    let proxy_task =
+        tokio::task::spawn_blocking(|| proxy_thread.join().expect("Failed to join thread"));
     let backend_task = tokio::task::spawn(MetadataBackend::run(ch_backend, client));
 
     let _ = tokio::try_join!(proxy_task, backend_task);
