3ca17c2030d447188a359b5a21d3b7fc6534ecbf
Esa Varemo <esa@kuivanto.fi>
2022-08-30 04:02:54 +0300

    Remove old CRD versions entirely

--- src/crd/utils.rs
+++ src/crd/utils.rs
@@ -1,8 +1,9 @@
-use crate::{Client, Error};
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
-use kube::api::{Patch, PatchParams};
+use kube::api::{Patch, PatchParams, PostParams};
 use kube::Api;
 use serde_json::json;
+
+use crate::{Client, Error};
 
 pub async fn remove_crd_version(
     crd_name: &str,
@@ -10,18 +11,21 @@ pub async fn remove_crd_version(
     client: Client,
 ) -> Result<(), Error> {
     let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
+
+    // Delete from .status.stored_versions
+
     let crd = crds.get(crd_name).await?;
-    let mut versions = crd
+    let mut stored_versions = crd
         .status
         .expect("CRD has no status")
         .stored_versions
         .expect("CRD has no stored versions");
 
-    versions.retain(|v| v != crd_version);
+    stored_versions.retain(|v| v != crd_version);
 
     let patch = json!({
         "status": {
-            "storedVersions": versions
+            "storedVersions": stored_versions
         }
     });
 
@@ -31,5 +35,11 @@ pub async fn remove_crd_version(
         &Patch::Merge(patch),
     )
     .await?;
+
+    // Delete from .spec.versions
+
+    let mut crd = crds.get(crd_name).await?;
+    crd.spec.versions.retain(|v| v.name != crd_version);
+    crds.replace(crd_name, &PostParams::default(), &crd).await?;
     Ok(())
 }
