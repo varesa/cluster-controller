d14929e9a5f445e107dfc401256f2d59dd7ace43
Esa Varemo <git@esav.fi>
2023-05-11 00:38:41 +0300

    Refactor ceph controllers to prepare for image controller

--- src/cluster/ceph/mod.rs
+++ src/cluster/ceph/mod.rs
@@ -1,12 +1,12 @@
-mod controller;
-
 use kube::Client;
 
 use crate::crd;
 use crate::errors::Error;
 
+mod controllers;
+
 pub async fn run(client: Client) -> Result<(), Error> {
     crd::ceph::create(client.clone()).await?;
-    controller::create(client.clone()).await?;
+    controllers::volumes::create(client.clone()).await?;
     Ok(())
 }
