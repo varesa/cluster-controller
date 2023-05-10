72b9019ca9b69ef375d87e3be4daf87c59686bd5
Esa Varemo <git@esav.fi>
2023-05-11 00:38:02 +0300

    Add CRD for Ceph Image

--- src/crd/ceph.rs
+++ src/crd/ceph.rs
@@ -12,7 +12,8 @@ use crate::utils::wait_crd_ready;
 #[derive(Debug, PartialEq, Eq, Clone, JsonSchema, Serialize, Deserialize, Default)]
 pub struct Quantity(String);
 
-const CRD_NAME: &str = "volumes.cluster-virt.acl.fi";
+const VOLUME_CRD_NAME: &str = "volumes.cluster-virt.acl.fi";
+const IMAGE_CRD_NAME: &str = "images.cluster-virt.acl.fi";
 
 #[derive(
     CustomResource, Serialize, Deserialize, Default, Debug, PartialEq, Eq, Clone, JsonSchema,
@@ -38,13 +39,43 @@ pub struct VolumeStatus {
     is_created: bool,
 }
 
+#[derive(
+    CustomResource, Serialize, Deserialize, Default, Debug, PartialEq, Eq, Clone, JsonSchema,
+)]
+#[kube(
+    group = "cluster-virt.acl.fi",
+    version = "v1beta",
+    kind = "Image",
+    status = "ImageStatus",
+    derive = "PartialEq",
+    derive = "Default",
+    shortname = "i",
+    namespaced
+)]
+pub struct ImageSpec {
+    pub source: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, JsonSchema)]
+pub struct ImageStatus {
+    size: usize,
+    is_allocated: bool,
+    is_imported: bool,
+    import_in_progress: bool,
+}
+
 pub async fn create(client: Client) -> Result<(), Error> {
     let crds: Api<CustomResourceDefinition> = Api::all(client.clone());
     let patch_params = PatchParams::apply("cluster-manager.ceph").force();
 
     let crd = Volume::crd();
-    crds.patch(CRD_NAME, &patch_params, &Patch::Apply(&crd))
+    crds.patch(VOLUME_CRD_NAME, &patch_params, &Patch::Apply(&crd))
         .await?;
-    wait_crd_ready(&crds, CRD_NAME).await?;
+    wait_crd_ready(&crds, VOLUME_CRD_NAME).await?;
+
+    let crd = Image::crd();
+    crds.patch(IMAGE_CRD_NAME, &patch_params, &Patch::Apply(&crd))
+        .await?;
+    wait_crd_ready(&crds, IMAGE_CRD_NAME).await?;
     Ok(())
 }
