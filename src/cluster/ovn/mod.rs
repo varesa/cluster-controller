3628089f44bde16d6ffdfaf77ecfc5d3ec0fca18
Esa Varemo <git@esav.fi>
2023-10-28 14:50:54 +0300

    Start all OVN controllers in parallel (fix regression)

--- src/cluster/ovn/mod.rs
+++ src/cluster/ovn/mod.rs
@@ -12,8 +12,32 @@ pub(crate) mod types;
 
 pub async fn run(client: Client) -> Result<(), Error> {
     crd::ovn::create(client.clone()).await?;
-    controllers::network::create(client.clone()).await?;
-    controllers::router::create(client.clone()).await?;
-    controllers::vm::create(client.clone()).await?;
+    println!("OVN: CRD created");
+
+    let client_clone = client.clone();
+    let network_task = tokio::task::spawn(async {
+        panic!(
+            "OVN network controller exited: {:?}",
+            controllers::network::create(client_clone).await
+        );
+    });
+
+    let client_clone = client.clone();
+    let router_task = tokio::task::spawn(async {
+        panic!(
+            "OVN router controller exited: {:?}",
+            controllers::router::create(client_clone).await
+        );
+    });
+
+    let client_clone = client.clone();
+    let vm_task = tokio::task::spawn(async {
+        panic!(
+            "OVN vm controller exited: {:?}",
+            controllers::vm::create(client_clone).await
+        );
+    });
+
+    let _ = tokio::try_join!(vm_task, network_task, router_task)?;
     Ok(())
 }
