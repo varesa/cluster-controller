d29203dddfa666f99995bb810132853e218f7160
Esa Varemo <git@esav.fi>
2023-10-28 03:24:26 +0300

    Update the libvirt node controller to new builder

--- src/cluster/libvirt/controller/node.rs
+++ src/cluster/libvirt/controller/node.rs
@@ -1,6 +1,5 @@
-use futures::StreamExt;
 use k8s_openapi::api::core::v1::Node;
-use kube::runtime::controller::{Action, Controller};
+use kube::runtime::controller::Action;
 use kube::{
     api::{Api, ListParams},
     Client, ResourceExt,
@@ -11,13 +10,12 @@ use tokio::time::Duration;
 use crate::cluster::libvirt::controller::{MAINTENANCE_ANNOTATION, MIGRATION_REQUEST_ANNOTATION};
 use crate::crd::libvirt::VirtualMachine;
 use crate::errors::Error;
+use crate::ok_and_requeue;
 use crate::utils::extend_traits::{ExtendResource, TryStatus};
-use crate::{create_controller, ok_and_requeue};
+use crate::utils::resource_controller::{DefaultState, ResourceControllerBuilder};
 
-/// State available for the reconcile and error_policy functions
-/// called by the Controller
-struct State {
-    client: Client,
+async fn delete_fn(_vm: Arc<Node>, _ctx: Arc<DefaultState>) -> Result<Action, Error> {
+    Ok(Action::await_change())
 }
 
 async fn request_reschedule_node_vms(node: &Node, client: Client) -> Result<(), Error> {
@@ -40,7 +38,7 @@ async fn request_reschedule_node_vms(node: &Node, client: Client) -> Result<(), 
 }
 
 /// Handle updates to nodes in the cluster
-async fn reconcile(node: Arc<Node>, ctx: Arc<State>) -> Result<Action, Error> {
+async fn update_fn(node: Arc<Node>, ctx: Arc<DefaultState>) -> Result<Action, Error> {
     let client = ctx.client.clone();
     let node = node.as_ref().to_owned();
     let name = node.name_unchecked();
@@ -58,16 +56,13 @@ async fn reconcile(node: Arc<Node>, ctx: Arc<State>) -> Result<Action, Error> {
     ok_and_requeue!(600)
 }
 
-fn error_policy(_object: Arc<Node>, _error: &Error, _ctx: Arc<State>) -> Action {
-    Action::requeue(Duration::from_secs(15))
-}
-
 pub async fn create(client: Client) -> Result<(), Error> {
-    let context = Arc::new(State {
-        client: client.clone(),
-    });
-    let nodes: Api<Node> = Api::all(client.clone());
     println!("libvirt: Starting node controller");
-    create_controller!(nodes, reconcile, error_policy, context);
+    ResourceControllerBuilder::new(client)
+        .with_default_state()
+        .with_default_error_policy()
+        .with_functions(update_fn, delete_fn)
+        .run()
+        .await;
     Ok(())
 }
